c90ca5ca38c791e93c8d64500dd5b208c96d19d5
bnew <brandon@globurg.com>
2017-09-23 16:01:09 -0400

    push final version of julia tour dates

--- tourdates.js
+++ tourdates.js
@@ -8,17 +8,31 @@ var sketchFrameRate;
 var i = 0;
 var bgColor;
 
-function setup(){
-    createCanvas(820,312);
-    animationScenes.push(new animationScene(new someObject("9/23 Paraguay")));
-    animationScenes.push(new animationScene(new someObject("9/30 San Diego")));
-    animationScenes.push(new animationScene(new someObject("10/05 Philadelphia")));
+var logo;
+var img;
 
-bgColor = color(50,200,100)
+function setup(){
+    createCanvas(500,500);
+    animationScenes.push(new animationScene(new someObject("Asuncion DollHouseDH\n09.23.17")));
+    animationScenes.push(new animationScene(new someObject("San Diego  CRSSD\n10.01.17")));
+    animationScenes.push(new animationScene(new someObject("Philadelphia Rumor\n10.05.17")));
+    
+    textSize(42);
+    bgColor = color(50,200,100)
+    img = loadImage("http://files.physical-digital.com/juliaHead2.png")
+    logo = new bouncingImage();
+    frameRate(30);
+//    saveFrames("juliaInsta","png",40,30);
+
 }
 
 function draw(){
+
     background(bgColor);
+        logo.bounce();
+
+
+
     animationScenes[i].run();
     if(!animationScenes[i].isOn) 
     {
@@ -26,12 +40,23 @@ function draw(){
         i = i%animationScenes.length;
         animationScenes[i].isOn = true;
     }
-    
-    
+    var frameCountNumbers;
+
+    if(frameCount < 1000){
+        if (frameCount < 10)
+            frameCountNumbers = "000"+frameCount;
+        else if (frameCount < 100)
+            frameCountNumbers = "00"+frameCount;
+        else
+            frameCountNumbers = "0"+frameCount;
+    }else frameCountNumbers = frameCount;
+                    
+    var imgname = "jInsta_" + frameCountNumbers;
+    saveCanvas(imgname, "png");    
 }
 
 function animationScene(theThing){
-    this.length = 100; //this is in frames
+    this.length = 200; //this is in frames
     this.isOn = true;
     this.someObjectForAnimating = theThing; //tour date, our animation
     this.timer = this.length;
@@ -45,22 +70,95 @@ function animationScene(theThing){
             this.isOn = false;
             this.timer = this.length;
         }
-
-        
         
     }
 }
 
 function someObject(msg){
     this.message = msg;
-    this.color = color(random(255),random(255),random(255))
+    this.color = colorPicker();
     this.x = width/2;
-    this.y = height/2;
+    this.y = (height/2- calculateTextHeight(this.message));
+    textFont("DisposableDroid BB");
     this.display = function()
     {
         bgColor = this.color
-        textSize(32);
+        lineBackground();
         textAlign(CENTER);
+        //font
         text(this.message,this.x,this.y)
+        //this.changeBackground(2);
+        
+    }
+    this.changeBackground = function(frameNumber){
+        if(frameCount % frameNumber ==0 )
+            this.color = color(colorPicker())
+
+    }
+    
+    this.calculateTextHeight = function(){
+        
     }
 }
+
+function colorPicker(){
+    return color(random(25,255),random(25,255),random(25,255));
+}
+
+function bouncingImage(){
+    this.img = loadImage("http://files.physical-digital.com/juliaHead2.png");
+    this.x = 10;
+    this.y = 10;
+    this.velocityX = 2;
+    this.velocityY = 2;
+    this.bounce = function(){
+        imageMode(CENTER)
+        image(img,this.x,this.y);
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+        if(this.x > width || this.x < 0)
+            this.velocityX = this.velocityX*-1;
+        if(this.y >= height || this.y <= 0)
+            this.velocityY = this.velocityY*-1;
+    }
+
+}
+
+function calculateTextHeight(message){
+    return ((textAscent()+textDescent())/2)*splitTokens(message, "\n").length;
+}
+
+
+function lineBackground(){
+    for(var theX = 0; theX < width; theX+=5){
+        stroke(colorPicker())
+        line(theX,0,theX,height);
+    }
+}
+/*
+void bounceWithColor() {
+    textFont(f);
+    float rad = textWidth(message)/2;
+    float h = ((textAscent()+textDescent())/2)*splitTokens(message, "\n").length ; //this sort of accounts for number of lines w bounce effect..
+    x = x + ( xspeed * xdirection );
+    y = y + ( yspeed * ydirection );
+
+    // Test to see if the shape exceeds the boundaries of the screen
+    // If it does, reverse its direction by multiplying by -1
+    if (x > width-rad || x < rad) {
+      xdirection *= -1;
+    }
+    if (y > height-h || y < h) {
+      ydirection *= -1;
+    }
+    /* debug
+     rectMode(CORNERS);
+     rect(x, y, textWidth(message), textAscent()+textDescent());
+     println(textWidth(message));
+     */
+    // Draw the shape
+//    fill(randomColor());
+//    text(message, x, y);
+//  }
+      
+      
